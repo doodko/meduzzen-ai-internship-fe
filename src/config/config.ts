dbc0d3748f74af484515fbba30193e88ab149fc3
Oleksandr Doodko <alexdoodko@gmail.com>
2025-06-14 10:05:14 +0300

    update sentry config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,6 @@
 export const config = {
+  ENVIRONMENT: process.env.NEXT_PUBLIC_ENVIRONMENT || "development",
   WS_URL: process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:5000/ws",
   API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000",
+  SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
 };
--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,17 +1,11 @@
-// This file configures the initialization of Sentry on the client.
-// The added config here will be used whenever a users loads a page in their browser.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from "@sentry/nextjs";
+import { config } from "@/config/config";
 
 Sentry.init({
-  dsn: "https://9b09d8b8b5a13a34bccaa9ebea06fc71@o4507192176934912.ingest.de.sentry.io/4509493032910928",
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  dsn: config.SENTRY_DSN,
   tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  debug: config.ENVIRONMENT === "local",
+  environment: config.ENVIRONMENT,
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
